e1807c8a11f90c4ff88f2e19971a50bac72010f0
Lauren Joseph <ljoseph.student@hsfi.us>
2017-03-21 21:12:06 +0000

    changes

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 
+var ScriptEduactors = ["Jenna","Mat","Nick"];
+    
+    console.log(ScriptEduactors[0]);
+var ScriptEders = ["Daril","Felix","Lauren","Isabela","Pamela","Anahi","Gabby"];
+    console.log(ScriptEders);
 
 
+$("#studentButton").click(function() {
+  var random = Math.floor(Math.random()*ScriptEders.length)  
+  console.log(ScriptEders[random])
+  $(studentDisplay).html(ScriptEders[random]);
+  
+});
+
+
+
+$("#teacherButton").click(function() {
+  var random = Math.floor(Math.random()*ScriptEduactors.length)  
+  console.log(ScriptEduactors[random])
+  $(teacherDisplay).html(ScriptEduactors[random]);
+  
+});
 });
